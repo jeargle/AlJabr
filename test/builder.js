2cd987ece32e69cb2a621ee6cc4dae37edc3bb1c
John Eargle <jeargle@gmail.com>
2015-07-10 23:06:13 -0500

    added element inspector and cayley graph panels

--- test/builder.js
+++ test/builder.js
@@ -5,6 +5,64 @@
 extend(aljabr, 'builder');
 
 
+aljabr.builder.CayleyTableView = aljabr.Class({
+    width: 0,
+    height: 0,
+    init: function(id) {
+        'use strict';
+        var view;
+
+        view = this;
+        view.id = id;
+        view.el = d3.select('#' + view.id);
+        view.width = 300;
+        view.height = 300;
+
+        view.render();
+    },
+    render: function() {
+        'use strict';
+        var view, svg;
+
+        view = this;
+        svg = view.el.append('svg')
+            .attr('width', view.width)
+            .attr('height', view.height);
+    }
+});
+
+aljabr.builder.cayleyTableView = null;
+
+
+aljabr.builder.ElementView = aljabr.Class({
+    width: 0,
+    height: 0,
+    init: function(id) {
+        'use strict';
+        var view;
+
+        view = this;
+        view.id = id;
+        view.el = d3.select('#' + view.id);
+        view.width = 300;
+        view.height = 300;
+
+        view.render();
+    },
+    render: function() {
+        'use strict';
+        var view, svg;
+
+        view = this;
+        svg = view.el.append('svg')
+            .attr('width', view.width)
+            .attr('height', view.height);
+    }
+});
+
+aljabr.builder.elementView = null;
+
+
 aljabr.builder.CayleyGraphView = aljabr.Class({
     width: 0,
     height: 0,
@@ -34,11 +92,14 @@ aljabr.builder.CayleyGraphView = aljabr.Class({
 aljabr.builder.cayleyGraphView = null;
 
 
+
 $(document).ready(function() {
     'use strict';
     var builder;
 
     builder = aljabr.builder;
 
-    builder.cayleyGraphView = new builder.CayleyGraphView('cayley-table');
+    builder.cayleyTableView = new builder.CayleyTableView('cayley-table');
+    builder.elementView = new builder.ElementView('element-inspector');
+    builder.cayleyGraphView = new builder.CayleyGraphView('cayley-graph');
 });
