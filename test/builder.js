0a3d86544d240e501ab83a7b247d18f7e5d48279
John Eargle <jeargle@gmail.com>
2015-07-11 12:41:59 -0500

    added grid to CayleyTableView

--- test/builder.js
+++ test/builder.js
@@ -6,6 +6,7 @@ extend(aljabr, 'builder');
 
 
 aljabr.builder.CayleyTableView = aljabr.Class({
+    model: undefined,
     width: 0,
     height: 0,
     init: function(id) {
@@ -15,19 +16,59 @@ aljabr.builder.CayleyTableView = aljabr.Class({
         view = this;
         view.id = id;
         view.el = d3.select('#' + view.id);
-        view.width = 300;
-        view.height = 300;
+        view.width = 500;
+        view.height = 400;
 
         view.render();
     },
     render: function() {
         'use strict';
-        var view, svg;
+        var view, svg, boxSize, order, i, j;
 
         view = this;
         svg = view.el.append('svg')
             .attr('width', view.width)
             .attr('height', view.height);
+        boxSize = 50;
+        order = 5;
+
+        for (i=0; i<order; i++) {
+            for (j=0; j<order; j++) {
+                svg.append('rect')
+                    .attr('x', (i+1)*boxSize)
+                    .attr('y', (j+1)*boxSize)
+                    .attr('width', boxSize)
+                    .attr('height', boxSize)
+                    .style('fill', 'rgb(' + i*50 + ',' + j*50 + ',0)');
+            }
+        }
+
+        for (i=1; i<=order+1; i++) {
+            svg.append('line')
+                .attr('x1', boxSize*i)
+                .attr('y1', boxSize)
+                .attr('x2', boxSize*i)
+                .attr('y2', boxSize*(order+1))
+                .style('stroke', 'black')
+                .style('stroke-width', '2');
+            svg.append('line')
+                .attr('x1', boxSize)
+                .attr('y1', boxSize*i)
+                .attr('x2', boxSize*(order+1))
+                .attr('y2', boxSize*i)
+                .style('stroke', 'black')
+                .style('stroke-width', '2');
+        }
+        
+
+        return view;
+    },
+    attach: function(model) {
+        'use strict';
+        var view;
+
+        view = this;
+        view.model = model;
     }
 });
 
@@ -44,7 +85,7 @@ aljabr.builder.ElementView = aljabr.Class({
         view = this;
         view.id = id;
         view.el = d3.select('#' + view.id);
-        view.width = 300;
+        view.width = 500;
         view.height = 300;
 
         view.render();
@@ -57,6 +98,8 @@ aljabr.builder.ElementView = aljabr.Class({
         svg = view.el.append('svg')
             .attr('width', view.width)
             .attr('height', view.height);
+
+        return view;
     }
 });
 
@@ -73,7 +116,7 @@ aljabr.builder.CayleyGraphView = aljabr.Class({
         view = this;
         view.id = id;
         view.el = d3.select('#' + view.id);
-        view.width = 300;
+        view.width = 500;
         view.height = 300;
 
         view.render();
@@ -86,6 +129,8 @@ aljabr.builder.CayleyGraphView = aljabr.Class({
         svg = view.el.append('svg')
             .attr('width', view.width)
             .attr('height', view.height);
+
+        return view;
     }
 });
 
